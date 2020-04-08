09ce4e81291feaad541485e92bf19aab94646074
Yushae <yushaefraza@gmail.com>
2020-04-08 15:50:33 -0600

    Updates

---
+++ precache-manifest.0f7bdf975561867c378333012f67625c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d60a3b222a97baca18cafac9311c455",
+    "url": "/shimpchat/index.html"
+  },
+  {
+    "revision": "3c93a4c03d36b29cecb7",
+    "url": "/shimpchat/static/css/main.8c7ff731.chunk.css"
+  },
+  {
+    "revision": "40e0a39bc4df2a576028",
+    "url": "/shimpchat/static/js/2.34aaca37.chunk.js"
+  },
+  {
+    "revision": "3b60cce9eb220e0eed2b11c719a93ed7",
+    "url": "/shimpchat/static/js/2.34aaca37.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c93a4c03d36b29cecb7",
+    "url": "/shimpchat/static/js/main.969ba99c.chunk.js"
+  },
+  {
+    "revision": "75efe288f3180ae4ec86",
+    "url": "/shimpchat/static/js/runtime-main.f7d1ff1c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/shimpchat/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "be90594a02d96f5923db55ee9e358857",
+    "url": "/shimpchat/static/media/simpBack.be90594a.png"
+  }
+]);
